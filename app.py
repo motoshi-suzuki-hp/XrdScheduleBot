4baad333c7ca2da84445216bb26f6cb4415fad93
Motoshi Suzuki <101000230+motoshi-suzuki-hp@users.noreply.github.com>
2023-11-22 18:55:43 +0900

    5

--- app.py
+++ app.py
@@ -1,83 +1,63 @@
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from flask_sqlalchemy import SQLAlchemy
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-import os
 
-#token取得---------------------------------------------------
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schedule.db'
+db = SQLAlchemy(app)
+
 line_bot_api = LineBotApi('caJal+9vjkxrQnDCoNvdIUJp+6RbN0CV8SevJdhx7VPmFcLWzI7xjsRhQ7RYmjm5LGhkUU9Co8gCp2hvOfarpHUmQpPqpsiMb6m8en7nzrF2w8PJJRGbcs0x6NhTEUfgUBZTa0vaodq9ZeBD8znLFAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('e18551997846615098b69cef7933efb2')
-#---------------------------------------------------------------
 
-
-#確認------------------------------
-app = Flask(__name__)
-app.debug = False
-
-@app.route("/")
-def say_hello():
-    return "Hello!!!!!"
-#----------------------------------
-
-
-#LINEbotを呼び出す---------------------------------
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
-#----------------------------------------------------------
 
-
-#ダジャレ開始--------------------------------------------------------
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
-    df_list=[['ふとん', 'ふとんがふっとんだ'],
-            ['みかん', 'アルミ缶のうえにあるみかん'],
-            ['ねこ', 'ねこが寝ころんだ'],
-            ['いす', 'いすに居座る'],
-            ['委員会', '委員会に入っていいんかい'],
-            ['いくら', 'このいくら、いくらですか？'],
-            ['飲酒', '飲酒していいんしゅか？'],
-            ['お金', 'お金はおっかねえ'],
-            ['カンニング', 'カンニングはいかんにんぐ']]
-
-    for i in range(len(df_list)):
-        joke=None
-        if event.message.text==df_list[i][0]:
-            joke=df_list[i][1]
+    user_message = event.message.text.split(',')
+    if len(user_message) == 3:
+        schedule_name, start_time, end_time = user_message
+        new_schedule = Schedule(name=schedule_name, start_time=start_time, end_time=end_time)
+        db.session.add(new_schedule)
+        try:
+            db.session.commit()
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=joke))
-        else:continue
-
-    if joke==None:
-        line_bot_api.reply_message(
+                TextSendMessage(text="予定が追加されました。")
+            )
+        except:
+            db.session.rollback()
+            line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text='すまん、思いつかんかった!!'))
+                TextSendMessage(text="予定が重複しています。")
+            )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="正しい形式で予定を入力してください。")
+        )
 
-#------------------------------------------------------------------------
+
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    start_time = db.Column(db.String(20), nullable=False)
+    end_time = db.Column(db.String(20), nullable=False)
+
+
 if __name__ == "__main__":
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(debug=True)
